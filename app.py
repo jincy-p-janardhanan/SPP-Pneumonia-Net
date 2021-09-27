6dd884abe7af7d0613ccb17b655b18f0272230c9
Jincy P Janardhanan <jincyp7@gmail.com>
2021-09-28 03:28:02 +0530

    File upload fixed.

--- app.py
+++ app.py
@@ -3,41 +3,29 @@ from flask import Flask,flash,redirect,url_for,render_template,request
 from werkzeug.utils import secure_filename
 import secrets
 
-UPLOAD_FOLDER = './static/images'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+UPLOAD_FOLDER = './static/uploads/'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
 secret = secrets.token_urlsafe(32)
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = secret
+
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'submit':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+    if request.method == 'POST':
+        file = request.files['inputfile']
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('upload-file',filename=filename))
+            file.save(app.config['UPLOAD_FOLDER']+ secure_filename(file.filename))
+            flash('Your results: ')
+        else:
+            flash('Unsupported file type!\nPlease upload file in .jpg, .jpeg, or .png formats only.')
     return render_template('index.html')
 
-@app.route('/result') 
-def result():
-    return render_template('result.html')
-
-
-    
 if __name__ == '__main__':
     app.run(debug=True)
