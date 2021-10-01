8393417e549cb6f3180ad9035fbb1ac2a1fe49ef
Jincy P Janardhanan <jincyp7@gmail.com>
2021-10-01 21:52:50 +0530

    Deploying using tflite

--- ml_model.py
+++ ml_model.py
@@ -1,8 +1,13 @@
-from tensorflow import keras
+import tensorflow as tf
+# from tensorflow import keras
 import cv2
 import numpy as np
 
-model = keras.models.load_model('saved_model_11')
+# model = keras.models.load_model('saved_model_11')
+
+# Deploying using tflite
+interpreter = tf.lite.Interpreter('model.tflite')
+interpreter.allocate_tensors()
 
 input_height = 128
 input_width = 128
@@ -20,9 +25,18 @@ def get_prediction(image_path):
     #Normalize the image - convert the each pixel value between 0 and 1
     img = img / 255
     img = np.reshape(img, (1, input_height, input_width, n_channels))
+    img = np.float32(img)
 
     # predict result
-    prediction = model.predict(img)
+    # prediction = model.predict(img)
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    interpreter.set_tensor(input_details[0]['index'], img)
+
+    interpreter.invoke()
+    prediction = interpreter.get_tensor(output_details[0]['index'])
+
+    print(prediction)
     result = np.argmax(prediction)
     label = labels[result]
     confidence = prediction[0][result] * 100
